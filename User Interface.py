c60ccab9237c27e5a5fa791b86d6ba48c24185a6
Jsronce <js82@live.missouristate.edu>
2015-02-07 13:23:01 -0600

    Added Subsurfaces(windows) for chatbox and UI

--- User Interface.py
+++ User Interface.py
@@ -5,20 +5,47 @@ from pygame.locals import *
 
 def window():
     pygame.init()
-    size = 200, 800
+    size = 800, 600
     speed = [2, 2]
     white = 255, 255, 255
-    screen = pygame.display.set_mode(size)
+    screen = pygame.display.set_mode(size)    
+    return screen
+    
+
+def chatBox(screen):
+    size_rect = pygame.Rect((0, 500), (600,100))
+    chatbox = screen.subsurface(size_rect)
+    return chatbox
+
+
+
+def userInterface(screen):
+    size_rect = pygame.Rect((600,0), (200, 600))
+    UI = screen.subsurface(size_rect)
+    return UI
+
+
+def game(windows):
+    screen = windows["screen"]
+    chatbox = windows["chatbox"]
+    UI = windows["userinterface"]
     while 1:
-        screen.fill(white)
+        chatbox.fill((255,255,255))
+        UI.fill((255,255,255))
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                sys.exit()
-
+                pygame.quit()
+                return 0
+    
 
 
 def main():
-    window()
+    windows = dict()
+    windows["screen"] = window()
+    screen = windows["screen"]
+    windows["chatbox"] = chatBox(screen)
+    windows["userinterface"] = userInterface(screen)
+    game(windows)
 
 main()
