431dee88fe336b62e45accc11b4947519972c5f5
Jennifer Burman <jburman28@gmail.com>
2015-02-07 22:52:04 -0600

    A few constants I thought we would want globally (may be moved later)

---
+++ globals.py
@@ -0,0 +1,19 @@
+
+class Globals(object):
+    
+    maroon = (80, 0, 0)
+    lightGray = (200, 200, 200)
+    black = (0, 0, 0)
+
+    collegeAbbr = {"Arts and Letters": "AL",
+                   "Business": "Bus",
+                   "Education": "Ed",
+                   "Health and Human Services": "HHS",
+                   "Humanities and Public Affairs": "HPA",
+                   "Natural and Applied Sciences": "NAS",
+                   "Agriculture": "Ag"}
+
+    
+
+
+    
