8923b8149fb9c6ba02a929f6d547d26938f3416e
Jennifer Burman <jburman28@gmail.com>
2015-02-08 04:34:31 -0600

    Added buildings owned and colored college names

--- playersDisplay.py
+++ playersDisplay.py
@@ -37,11 +37,23 @@ class PlayersDisplay(object):
         text = font.render(self.players[i].getName(),
                            True, Color('black'))
         self.background.blit(text, (0, 85*i))
-        
-        text = font.render(self.players[i].getCollegeAbbr(),
+
+        college = self.players[i].getCollege()
+
+        # For the colleges with light colors, create a black outline.
+        if college == 'Natural and Applied Sciences' \
+           or college == 'Health and Human Services' \
+           or college == 'Education' or college == 'Business':
+            textOutline = font.render(Globals.collegeAbbr[college],
                            True, Color('black'))
+            self.background.blit(textOutline, (74, 85*i - 1))
+            self.background.blit(textOutline, (74, 85*i + 1))
+            self.background.blit(textOutline, (76, 85*i - 1))
+            self.background.blit(textOutline, (76, 85*i + 1))
+            
+        text = font.render(Globals.collegeAbbr[college],
+                           True, Globals.collegeColors[college])
         self.background.blit(text, (75, 85*i))
-        #pygame.draw.circle(self.background, black, (125, 5), 5)  # Do we want an icon?
 
         text = font.render('$' + str(self.players[i].getDollars()),
                            True, Color('black'))
@@ -49,11 +61,21 @@ class PlayersDisplay(object):
         
         text = font.render(str(self.players[i].getPoints()) + ' pts',
                            True, Color('black'))
-        self.background.blit(text, (20, 20+85*i))
+        self.background.blit(text, (20, 18+85*i))
 
         text = font.render(str(self.players[i].getPointsPerRound()) + ' pts/round',
                            True, Color('black'))
-        self.background.blit(text, (110, 20+85*i))
+        self.background.blit(text, (110, 18+85*i))
+
+        x = 0   # x position to place text of each building
+        y = 36  # y position to place text (within player's section)
+        for building in self.players[i].getBuildings():
+            text = font.render(Globals.buildingAbbr[building], True, Color('black'))
+            self.background.blit(text, (x, y+85*i))
+            x += 50
+            if x >= 200:
+                x = 0
+                y += 16
         
 
     def insert(self, x, y):
@@ -65,11 +87,25 @@ class PlayersDisplay(object):
 
 def main():
 
-    p1 = Player("player1", "Business")
+    p1 = Player("player1", "Agriculture")
     p2 = Player("player2", "Arts and Letters")
     p3 = Player("player3", "Natural and Applied Sciences")
-
-    players = [p1, p2, p3]
+    p4 = Player("player4", "Health and Human Services")
+    p5 = Player("player5", "Humanities and Public Affairs")
+    p6 = Player("player6", "Education")
+    
+    p1.addBuilding('Siceluff')
+    p1.addBuilding('Cheek')
+    p1.addBuilding('Plaster Student Union')
+    p1.addBuilding('Kemper')
+    p1.addBuilding('Glass')
+    p1.addBuilding('Strong')
+    p1.addBuilding('Karls')
+    p1.addBuilding('Ellis')
+    p1.addBuilding('JQH Arena')
+    p2.addBuilding('Temple')
+    
+    players = [p1, p2, p3, p4, p5, p6]
 
     pd = PlayersDisplay(players)
     pd.insert(0, 0)
