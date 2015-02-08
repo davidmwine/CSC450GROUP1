2b53499c331c1f337bd4dfe72eb7b479e0b03da4
Jennifer Burman <jburman28@gmail.com>
2015-02-08 04:32:22 -0600

    Added list of buildings (subject to change) and dictionary of building abbreviations

--- globals.py
+++ globals.py
@@ -5,13 +5,58 @@ class Globals(object):
     lightGray = (200, 200, 200)
     black = (0, 0, 0)
 
-    collegeAbbr = {"Arts and Letters": "AL",
-                   "Business": "Bus",
-                   "Education": "Ed",
-                   "Health and Human Services": "HHS",
-                   "Humanities and Public Affairs": "HPA",
-                   "Natural and Applied Sciences": "NAS",
-                   "Agriculture": "Ag"}
+    collegeColors = {'Arts and Letters': (128, 0, 128),  # Purple
+                   'Business': (255, 0, 0),           # Red
+                   'Education': (173, 216, 230),           # Light Blue
+                   'Health and Human Services': (255, 140, 0),  # Orange
+                   'Humanities and Public Affairs': (0, 128, 128),  # Peacock Blue
+                   'Natural and Applied Sciences': (255, 215, 0),   # Golden Yellow
+                   'Agriculture': (0, 128, 0)}         # Green
+    
+    collegeAbbr = {'Arts and Letters': 'AL',
+                   'Business': 'Bus',
+                   'Education': 'Ed',
+                   'Health and Human Services': 'HHS',
+                   'Humanities and Public Affairs': 'HPA',
+                   'Natural and Applied Sciences': 'NAS',
+                   'Agriculture': 'Ag'}
+
+    buildings = ['Carrington', 'Siceluff', 'Cheek', 'Bookstore',
+                 'Plaster Student Union', 'McDonald Arena', 'Meyer Library',
+                 'Foster Recreation Center', 'Hammons Field', 'Brick City',
+                 'Bear Park North', 'Juanita K Hammons', 'JQH Arena',
+                 'Hammons Student Center', 'Allison South', 'Glass', 'Strong',
+                 'Bear Park South', 'Kemper', 'Temple', 'Plaster Stadium',
+                 'Karl', 'Pummil', 'Craig', 'Ellis', 'Hill']
+
+    buildingAbbr = {'Carrington': 'CARR',
+                    'Siceluff': 'SICL',
+                    'Cheek': 'CHEEK',
+                    'Bookstore': 'BOOK',
+                    'Plaster Student Union': 'PSU',
+                    'McDonald Arena': 'MCDA',
+                    'Meyer Library': 'LIBR',
+                    'Foster Recreation Center': 'FRC',
+                    'Hammons Field': 'HAMM',
+                    'Brick City': 'BRICK',
+                    'Bear Park North': 'BPN',
+                    'Juanita K Hammons': 'JKH',
+                    'JQH Arena': 'JQH',
+                    'Hammons Student Center': 'HSC',
+                    'Allison South': 'ALL',
+                    'Glass': 'GLASS',
+                    'Strong': 'STRO',
+                    'Bear Park South': 'BPS',
+                    'Kemper': 'KEMP',
+                    'Temple': 'TEMP',
+                    'Plaster Stadium': 'PLAS',
+                    'Karls': 'KARL',
+                    'Pummil': 'PUMM',
+                    'Craig': 'CRAIG',
+                    'Ellis': 'ELLIS',
+                    'Hill': 'HILL'}
+
+    
 
     
 
