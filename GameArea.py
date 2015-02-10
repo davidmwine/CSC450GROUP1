a7367062e189f07ed67fde6632a59240522fa871
Jsronce <js82@live.missouristate.edu>
2015-02-09 19:47:00 -0600

    Game area update.

--- GameArea.py
+++ GameArea.py
@@ -1,7 +1,7 @@
 import pygame, sys
 from pygame.locals import *
 from playersDisplay import PlayersDisplay
-
+from player import Player
 
 
 class gameArea():
@@ -16,12 +16,13 @@ class gameArea():
             self.area = pygame.Surface((self.width, self.height))
         else:
             pygame.init()
-            self.screen = pygame.display.set_mode((self.width, self.height))
+            self.area = pygame.display.set_mode((self.width, self.height))
             
 
-        self.playersDisplay = PlayersDisplay(players, scale, 1)
+        self.playersDisplay = PlayersDisplay(testplayers(), scale, 1)
+        
         size_rect = pygame.Rect((0, 900), (1400,180))
-        self.chatbox = self.area.subscreen(size_rect)
+        self.chatbox = self.area.subsurface(size_rect)
         
         #self.controls
         #self.board
@@ -36,14 +37,48 @@ class gameArea():
 
 
     def play(self):
-       while 1:
-        self.chatbox.fill((255,255,255))
-        pygame.display.flip()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return 0 
+        rect = pygame.Rect((1440,0), (480, 810))
+        self.area.blit(PlayersDisplay.getPD(), rect)
+        while 1:
+            self.chatbox.fill((255,255,255))
+            pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return 0 
         
+
+def testplayers():
+    p1 = Player("player1", "Agriculture")
+    p2 = Player("player2", "Arts and Letters")
+    p3 = Player("player3", "Natural and Applied Sciences")
+    p4 = Player("player4", "Education")
+    p5 = Player("player5", "Health and Human Services")
+    p6 = Player("player6", "Humanities and Public Affairs")
+    
+    p1.addBuilding('Siceluff')
+    p1.addBuilding('Cheek')
+    p1.addBuilding('Plaster Student Union')
+    p1.addBuilding('Kemper')
+    p1.addBuilding('Strong')
+    p1.addBuilding('Karls')
+    p1.addBuilding('Glass')
+    p1.addBuilding('Ellis')
+    p1.addBuilding('JQH Arena')
+    p1.addBuilding('McDonald Arena')
+    p1.addBuilding('Meyer Library')
+    p1.addBuilding('Foster Recreation Center')
+    p1.addBuilding('Juanita K Hammons')
+    p1.addBuilding('Pummil')
+    p1.addBuilding('Plaster Stadium')
+    p1.addBuilding('Temple')
+    p2.addBuilding('Craig')
+    p2.addBuilding('Ellis')
+
+    p2.addDollars(30000000)
+    
+    return [p1, p2, p3, p4, p5, p6]
+
 
 
     
