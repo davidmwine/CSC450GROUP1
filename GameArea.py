930e6ebbe1578b153eb1d935281642395308a81a
Jsronce <js82@live.missouristate.edu>
2015-02-09 18:56:23 -0600

    Starting Game area

---
+++ GameArea.py
@@ -0,0 +1,15 @@
+import pygame, sys
+from pygame.locals import *
+
+
+
+
+class gameArea():
+
+
+    def __init__(self, scale, is_child=False):
+
+        if ischild:
+            pygame.init.display.set_mode(size)
+        else:
+            
