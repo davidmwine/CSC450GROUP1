4546b64bfc322734c20d3396e49ed61a3098c3a5
Jsronce <js82@live.missouristate.edu>
2015-02-16 09:52:13 -0600

    Chat box text

--- ChatBox.py
+++ ChatBox.py
@@ -22,25 +22,25 @@ class chatBox():
         self.area.fill((0,255,0))
         self.chatenter = chatEnter(self.area,Rect(0 , self.line_height*8,
         self.line_width, self.line_height), None)
-        for i in range(7):
+        for i in range(0,):
             line_rect = Rect(0, i*self.line_height,
                         self.line_width, self.line_height)
             print(line_rect)
-            self.chatlines.append(chatLine( self.area,line_rect
-                                        ,"Line {}".format(i)))
+            self.chatlines.append(
+            chatLine( self.area, line_rect, "Line {}".format(i)))
                 
 
 class chatLine():
-    def __init__(self, parent, rect ,string= ""):
+    def __init__(self, parent, rect ,string= " "):
         self._text = string
         self._width = rect.right - rect.left    
-        self._height = rect.top - rect.bottom
+        self._height = rect.bottom - rect.top
         self._area = parent.subsurface(rect)
-        self._font = pygame.font.Font( None, int(self._height))
-        self._textarea = self._font.render( self._text, 1, (255,255,255) )
-        print(self._textarea, rect, self._text)
-        self._area.convert()
-        self._area.blit(self._textarea,rect)
+        self._area.fill((255,255,255))
+        self._font = pygame.font.Font( None, 20)
+        self._textarea = self._font.render( self._text, 1, (0,0,0) )
+        print(self._textarea,self._area)
+        self._area.blit(self._textarea,(0,0))
         
 
     def get_area(self):
