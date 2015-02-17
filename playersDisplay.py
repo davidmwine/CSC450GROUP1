c7bdc714059b6675c13202385c8fee215d313ab4
Jennifer Burman <jburman28@gmail.com>
2015-02-16 21:36:50 -0600

    Removed list of buildings and made some visual changes.

--- playersDisplay.py
+++ playersDisplay.py
@@ -36,6 +36,12 @@ class PlayersDisplay(object):
         self.height = int(c*810)
         self.playerHeight = int(c*135)
 
+        # Colors
+        maroon = (80, 0, 0)
+        lightGray = (200, 200, 200)
+        medGray = (180, 180, 180)
+        
+        # If this isn't called by another screen, make this the main screen.
         if parent == False:
             pygame.init()
             self.screen = pygame.display.set_mode((self.width, self.height))
@@ -43,16 +49,20 @@ class PlayersDisplay(object):
         # The main Surface for the PlayersDisplay, which we'll keep adding to
         self.pd = pygame.Surface((self.width, self.height))
         self.pd = self.pd.convert()
-        self.pd.fill(Globals.maroon)
+        self.pd.fill(maroon)
 
+        # Add gray background for player data & separate with lines.
         totalPlayersHeight = self.playerHeight * len(players)
-        pygame.draw.rect(self.pd, Globals.lightGray,
+        pygame.draw.rect(self.pd, lightGray,
                          (0, 0, self.width, totalPlayersHeight), 0)
-        for i in range(1, len(players)):
-            pygame.draw.lines(self.pd, Globals.maroon, False,
+        for i in range(0, len(players)):
+            pygame.draw.rect(self.pd, medGray,
+                         (0, i*self.playerHeight, self.width, 45*c), 0)
+            pygame.draw.lines(self.pd, maroon, False,
                               [(0, self.playerHeight*i),
-                               (self.width, self.playerHeight*i)], 1)
-    
+                               (self.width, self.playerHeight*i)], 2)
+
+        # Add text.
         if pygame.font:
             for i in range(len(players)):
                 self.printText(c, i)
@@ -60,13 +70,14 @@ class PlayersDisplay(object):
 
     def printText(self, c, i):
 
-        font = pygame.font.Font(None, int(30*c))
-        buildingFont = pygame.font.Font(None, int(26*c))
-             
+        font = pygame.font.Font(None, int(40*c))
+
+        # Player name  
         text = font.render(self.players[i].getName(),
                            True, Color('black'))
-        self.pd.blit(text, (2, self.playerHeight*i + 2))
+        self.pd.blit(text, (2, self.playerHeight*i + 4))
 
+        # College name
         college = self.players[i].getCollege()
         
         # For the colleges with light colors, create a black outline.
@@ -75,38 +86,31 @@ class PlayersDisplay(object):
            or college == 'Education' or college == 'Business':
             textOutline = font.render(Globals.collegeAbbr[college],
                            True, Color('black'))
-            self.pd.blit(textOutline, (140*c - 1, self.playerHeight*i + 1))
-            self.pd.blit(textOutline, (140*c - 1, self.playerHeight*i + 3))
-            self.pd.blit(textOutline, (140*c + 1, self.playerHeight*i + 1))
-            self.pd.blit(textOutline, (140*c + 1, self.playerHeight*i + 3))
+            self.pd.blit(textOutline, (220*c - 1, self.playerHeight*i + 5))
+            self.pd.blit(textOutline, (220*c - 1, self.playerHeight*i + 3))
+            self.pd.blit(textOutline, (220*c + 1, self.playerHeight*i + 5))
+            self.pd.blit(textOutline, (220*c + 1, self.playerHeight*i + 3))
         
         text = font.render(Globals.collegeAbbr[college],
                            True, Globals.collegeColors[college])
-        self.pd.blit(text, (140*c, self.playerHeight*i + 2))
-        
-        text = font.render('${:,d}'.format(self.players[i].getDollars()),
-                           True, Color('black'))
-        self.pd.blit(text, (350*c, self.playerHeight*i + 2))
-        
+        self.pd.blit(text, (220*c, self.playerHeight*i + 4))
+
+        # Points
         text = font.render(str(self.players[i].getPoints()) + ' pts',
                            True, Color('black'))
-        self.pd.blit(text, (20*c, self.playerHeight*i + 30*c))
-
+        self.pd.blit(text, (20*c, self.playerHeight*i + 50*c))
+        
+        # Points per round
         text = font.render(str(self.players[i].getPointsPerRound()) + ' pts/round',
                            True, Color('black'))
-        self.pd.blit(text, (250*c, self.playerHeight*i + 30*c))
-        
-        x = 2   # x position to place text of each building
-        y = 60*c  # y position to place text (within player's section)
-        for building in self.players[i].getBuildings():
-            text = buildingFont.render(building.getAbbr(), True, Color('black'))
-            self.pd.blit(text, (x, y + self.playerHeight*i))
-            x += 75*c
-            if x >= self.width - 60*c:
-                x = 2
-                y += 20*c
-         
+        self.pd.blit(text, (250*c, self.playerHeight*i + 50*c))
 
+        # Dollars
+        text = font.render('${:,d}'.format(self.players[i].getDollars()),
+                           True, Color('black'))
+        self.pd.blit(text, (20*c, self.playerHeight*i + 90*c))
+
+              
     def getPD(self):
         return self.pd
     
@@ -121,29 +125,11 @@ def main():
     p5 = Player("player5", "Health and Human Services")
     p6 = Player("player6", "Humanities and Public Affairs")
     
-    p1.addBuilding( Building('Cheek', 'CHEEK', 2) )
-    p1.addBuilding( Building('Meyer Library', 'LIBR', 6) )
-    p1.addBuilding( Building('Plaster Student Union', 'PSU', 4) )
-    p1.addBuilding( Building('Siceluff', 'SICL', 1) )
-    p1.addBuilding( Building('Kemper', 'KEMP', 18) )
-    p1.addBuilding( Building('Strong', 'STRO', 16) )
-    p1.addBuilding( Building('Karls', 'KARL', 21) )
-    p1.addBuilding( Building('Glass', 'GLASS', 15) )
-    p1.addBuilding( Building('JQH Arena', 'JQH', 12) )
-    p1.addBuilding( Building('McDonald Arena', 'MCDA', 5) )
-    p1.addBuilding( Building('Foster Recreation Center', 'FRC', 7) )
-    p1.addBuilding( Building('Juanita K Hammons', 'JKH', 11) )
-    p1.addBuilding( Building('Pummil', 'PUMM', 22) )
-    p1.addBuilding( Building('Plaster Stadium', 'PLAS', 20) )
-    p1.addBuilding( Building('Temple', 'TEMP', 19) )
-    p2.addBuilding( Building('Ellis', 'ELLIS', 24) )
-    p2.addBuilding( Building('Craig', 'CRAIG', 23) )
-    
     p2.addDollars(120000000)
     
     players = [p1, p2, p3, p4, p5, p6]
     
-    pd = PlayersDisplay(players, 1)
+    pd = PlayersDisplay(players, 0.7)
 
     pd.screen.blit(pd.getPD(), (0, 0))
     pygame.display.flip()
