af135915912fcf6742f8d2342c7e094b02caf5a0
Jennifer Burman <jburman28@gmail.com>
2015-02-20 16:47:24 -0600

    Version with rectangular game board and margin

---
+++ GameArea.py
@@ -0,0 +1,117 @@
+import pygame, sys
+from pygame.locals import *
+from playersDisplay import PlayersDisplay
+from player import Player
+from building import Building
+from board import GameBoard
+from Controls import Controls
+from ChatBox import chatBox
+
+
+class GameArea(object):
+
+
+    def __init__(self, parent=False, scale=1):
+
+        self.width = int(scale*1920)
+        self.height = int(scale*1080)
+        self.scale = scale
+        self.parent = parent
+
+        if self.parent:
+            self.area = pygame.Surface((self.width, self.height))
+        else:
+            pygame.init()
+            self.area = pygame.display.set_mode((self.width, self.height))
+
+        # Game Board
+        rect = pygame.Rect((0, 0), (1440*self.scale, 1020*self.scale))
+        self.boardArea = self.area.subsurface(rect)
+        self.boardArea.fill((94, 0, 9)) # official MSU maroon
+
+        self.gameBoard = GameBoard(self.scale, True)
+            
+        # Players Display
+        self.playerDis = PlayersDisplay(testplayers(), self.scale, True)
+
+        # Chat Box
+        rect = pygame.Rect((1440*self.scale, 810*self.scale),
+                           (480*self.scale, 270*self.scale))
+        self.chatbox = chatBox(1,self.area, rect)
+        
+        # Controls
+        rect = pygame.Rect((0, 1020*self.scale),
+                           (1440*self.scale, 60*self.scale))
+        self.controls = Controls(self.area, rect)
+
+
+    def get_area(self):
+        return self.area
+
+    def getScale(self):
+        return self.scale
+
+
+    def play(self):
+        game_exit = False
+
+        # Insert Players Display
+        rect = pygame.Rect((1440*self.scale, 0),
+                           (480*self.scale, 810*self.scale))
+        self.area.blit(self.playerDis.getPD(), rect)
+
+        # Insert Game Board
+        rect = pygame.Rect((20*self.scale, 20*self.scale),
+                           (1400*self.scale, 980*self.scale))
+        self.area.blit(self.gameBoard.getGB(), rect)
+
+        if self.parent:
+            self.parent.blit(self.area, (0,0))
+        
+        while not game_exit:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN: #TEMPORARY, will later replace with proper game exit
+                    if event.key == K_ESCAPE:
+                        game_exit = True
+                        break
+                elif event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                    return 0
+            pygame.display.update()
+        return "start"
+        
+
+def testplayers():
+    p1 = Player("player1", "Agriculture")
+    p2 = Player("player2", "Arts and Letters")
+    p3 = Player("player3", "Natural and Applied Sciences")
+    p4 = Player("player4", "Education")
+    p5 = Player("player5", "Health and Human Services")
+    p6 = Player("player6", "Humanities and Public Affairs")
+        
+    p2.addDollars(120000000)
+    
+    return [p1, p2, p3, p4, p5, p6]
+
+
+
+def main():
+    screen = GameArea(False, 2/3)
+    screen.play()
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+        
+        
+            
