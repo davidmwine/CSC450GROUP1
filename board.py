4471fb3f225ae45118be524ec2236ab543707c01
Dennis Minear <Minear563@live.missouristate.edu>
2015-03-18 19:57:59 -0500

    Renamed Globals to Colors; Merged Building and Buildings into one file.
    From Code Review

--- board.py
+++ board.py
@@ -1,100 +1,169 @@
 # Drawing the basic board layout
 
 import pygame
+from pygame.locals import *
 import sys
 import math
-from building import Building
-from pygame.locals import *
-  
-MAROON   = (80, 0, 0)                               # RGB code for Maroon       (University color - background)
-LTGRAY   = (200, 200, 200)                          # RGB code for light Gray   (Unowned)
-PURPLE   = (128, 0, 128)                            # RGB code for Purple       (Arts & Letters)
-RED      = (255, 0, 0)                              # RGB code for Red          (Business)
-LTBLUE   = (173, 216, 230)                          # RGB code for Light Blue   (Education)
-ORANGE   = (255, 140, 0)                            # RGB code for Orange       (Health & Human Services)
-PEACOCK  = (0, 128, 128)                            # RGB code for Peacock Blue (Humanities & Public Affairs)
-GOLDEN   = (255, 215, 0)                            # RGB code for Golden Yellow (Natural and Applied Science)
-LTGREEN  = (0, 128, 0)                              # RGB code for Green        (Agriculture)
-BLACK    = (0, 0, 0)                                # RGB code for Black        
-WHITE    = (255, 255, 255)                          # RGB code for White        
-BLUE     = (0, 0, 255)                              # RGB code for Blue         
-GREEN    = (0, 255, 0)                              # RGB code for Green        
+import Building
+import Colors 
 
-# Width and height of each grid location
-size = [800, 600]                                   # set the overall size
-margin = 2                                          # sets the margin between each cell
-padding = 3                                         # sets the padding for text
-width  = (math.floor(size[0]/10)-margin)            # set the Width of each cell
-height = (math.floor(size[1]/10)-margin)            # set the Height of each cell
+class GameBoard(object):
 
-print (size, width, height)                                                                     # debugging code
+    def __init__(self, scale, buildings, isSubscreen=False):
 
-buildings = []
-boardorder = 0
-buildings.append( Building('Carrington Hall', boardorder+len(buildings), 'special') )
-buildings.append( Building('Siceluff Hall', boardorder+len(buildings), 'academic') )
-buildings.append( Building('Cheek Hall', boardorder+len(buildings), 'academic') ) 
-buildings.append( Building('Wells House', boardorder+len(buildings), 'support') ) 
-buildings.append( Building('University Bookstore', boardorder+len(buildings), 'support') ) 
-buildings.append( Building('Hammons Field', boardorder+len(buildings), 'sports') ) 
-buildings.append( Building('Greenwood Lab School', boardorder+len(buildings), 'academic') )
-buildings.append( Building('Blair-Shannon House', boardorder+len(buildings), 'support') )
-buildings.append( Building('Foster Recreation Center', boardorder+len(buildings), 'support') )
-buildings.append( Building('Bear Park North', boardorder+len(buildings), 'special') )
-buildings.append( Building('Pummill Hall', boardorder+len(buildings), 'academic') )
-buildings.append( Building('Juanita K Hammons', boardorder+len(buildings), 'support') )
-buildings.append( Building('Hill Hall', boardorder+len(buildings), 'academic') )
-buildings.append( Building('JQH Arena', boardorder+len(buildings), 'sports') )
-buildings.append( Building('Ellis Hall', boardorder+len(buildings), 'academic') )
-buildings.append( Building('Hammons Student Center', boardorder+len(buildings), 'support') )
-buildings.append( Building('Craig Hall', boardorder+len(buildings), 'academic') ) 
-buildings.append( Building('Allison South Stadium', boardorder+len(buildings), 'sports') )
-buildings.append( Building('Art Annex', boardorder+len(buildings), 'support') )
-buildings.append( Building('Brick City', boardorder+len(buildings), 'academic') ) 
-buildings.append( Building('Karls Hall', boardorder+len(buildings), 'academic') ) 
-buildings.append( Building('Kings St Annex', boardorder+len(buildings), 'academic') ) 
-buildings.append( Building('Meyer Library', boardorder+len(buildings), 'support') )
-buildings.append( Building('Forsythe Athletics Center', boardorder+len(buildings), 'support') ) 
-buildings.append( Building('Power House', boardorder+len(buildings), 'support') ) 
-buildings.append( Building('Freudenberger House', boardorder+len(buildings), 'support') ) 
-buildings.append( Building('Bear Park South', boardorder+len(buildings), 'special') )
-buildings.append( Building('Plaster Sports Complex', boardorder+len(buildings), 'sports') )
-buildings.append( Building('Central Stores & Maintenance', boardorder+len(buildings), 'support') ) 
-buildings.append( Building('Temple Hall', boardorder+len(buildings), 'academic') )      
-buildings.append( Building('Kemper Hall', boardorder+len(buildings), 'academic') ) 
-buildings.append( Building('Strong Hall', boardorder+len(buildings), 'academic') ) 
-buildings.append( Building('Glass Hall', boardorder+len(buildings), 'academic') ) 
-buildings.append( Building('McDonald Arena', boardorder+len(buildings), 'academic') )  
-buildings.append( Building('Plaster Student Union', boardorder+len(buildings), 'support') )
-buildings.append( Building('Accreditation Review', boardorder+len(buildings), 'special') )
-
-nullspace = boardorder+len(buildings)
-buildings.append( Building('', nullspace, 'nullspace') )            # Null Space 
-  
-pygame.init()
-board = pygame.display.set_mode(size)
-pygame.display.set_caption("Mastering MSU")              #Name of title
-boardfont = pygame.font.Font(None, 16)
-
-boardSpace = 0
-# Create a 2 dimensional array/list of lists.
-# The outer ( or rows ) dimension
-grid = []
-for row in range(10):
-    # The inner ( or columns ) dimension
-    grid.append([])
-    for column in range(10):
-        if row == 0 or row == 9 or column == 0 or column == 9:
-            grid[row].append(buildings[boardSpace])
-            mine = grid[row][column]
-            grid[row][column].setPosition([row, column])
-            boardSpace += 1
-        else:
-            grid[row].append(buildings[nullspace])
-            mine = grid[row][column]
-            grid[row][column].setPosition([row, column])
-        color = grid[row][column].getColor()
-        pygame.draw.rect(board, color, [(margin+width)*column+margin,(margin+height)*row+margin, width,height])
-        board.blit(boardfont.render(mine.getName(), True, BLACK), [(margin+width)*column+margin,((margin+height)+1)*row+margin, width,height])
+        # Width and height of each grid location
+        self.scale = scale
+        size = [int(self.scale*1400), int(self.scale*980)]  # set the overall size
+        margin = 2                                          # sets the margin between each cell
+        padding = 3                                         # sets the padding for text
+        polysize = 20
         
-pygame.display.flip()                           #Updates the screen
+        width  = (math.floor(size[0]/10)-margin)            # set the Width of each cell
+        height = (math.floor(size[1]/10)-margin)            # set the Height of each cell
+
+        padWidth = (size[0] - (width * 10)) / 2
+        padHeight = (size[1] - (height * 10)) / 2
+        
+        self.buildings = buildings
+
+        if isSubscreen == False:
+            pygame.init()
+            self.board = pygame.display.set_mode(size)
+            pygame.display.set_caption("Mastering MSU")              #Name of title
+        else:
+            self.board = pygame.Surface(size)
+            
+        self.boardfont = pygame.font.Font(None, 16)
+
+        x_pos = 0 + padWidth
+        y_pos = 0 + padHeight
+        angle = -90.0
+        
+        for building in range(len(buildings)):
+            seq = buildings[building].getSequence()
+            cellColor = buildings[building].getColor()
+            cellName = buildings[building].getName()
+            
+            print(seq, cellColor, cellName)
+                
+            # Side 1 (Top of screen)
+            if seq / 8.0 < 1:
+                side = 1
+                if seq % 8 == 0:
+                    cellWidth = width * 1.5
+                    cellHeight = height * 1.5
+                else:
+                    cellWidth = width
+                    cellHeight = height * 1.5
+                    pointList = (x_pos, y_pos + cellHeight),(x_pos + polysize, y_pos + cellHeight + polysize), (x_pos + cellWidth - polysize, y_pos + cellHeight + polysize), (x_pos + cellWidth, y_pos + cellHeight)
+                    poly = pygame.draw.polygon(self.board, cellColor, pointList)
+                    poly = pygame.draw.polygon(self.board, Colors.BLACK, pointList, 1)
+                    # Store pointList in building object
+                    buildings[building].setPointList(pointList)
+                    
+                rect = pygame.draw.rect(self.board, cellColor, (x_pos, y_pos, cellWidth, cellHeight))
+                rect = pygame.draw.rect(self.board, Colors.BLACK, (x_pos, y_pos, cellWidth, cellHeight),margin)
+                # Store rect in building object
+                buildings[building].setRect(rect)
+                x_pos += cellWidth
+                        
+            # Side 2 (Right of screen)
+            elif seq / 8.0 >= 1 and seq / 8.0 < 2:
+                side = 2
+                if seq % 8 == 0:
+                    cellWidth = width * 1.5
+                    cellHeight = height * 1.5
+                else:
+                    cellWidth = width * 1.5
+                    cellHeight = height
+                    pointList = (x_pos, y_pos),(x_pos - polysize, y_pos + polysize), (x_pos - polysize, y_pos + cellHeight - polysize), (x_pos, y_pos + cellHeight)
+                    poly = pygame.draw.polygon(self.board, cellColor, pointList)
+                    poly = pygame.draw.polygon(self.board, Colors.BLACK, pointList, 1)
+                    # Store pointList in building object
+                    buildings[building].setPointList(pointList)
+                    
+                rect = pygame.draw.rect(self.board, cellColor, (x_pos, y_pos, cellWidth, cellHeight))
+                rect = pygame.draw.rect(self.board, Colors.BLACK, (x_pos, y_pos, cellWidth, cellHeight),margin)
+                # Store rect in building object
+                buildings[building].setRect(rect)
+                y_pos += cellHeight
+                            
+            # Side 3 (Bottom of screen)
+            elif seq / 8.0 >= 2 and seq / 8.0 < 3:
+                side = 3
+                if seq % 8 == 0:
+                    cellWidth = width * 1.5
+                    cellHeight = height * 1.5
+                else:
+                    cellWidth = width
+                    cellHeight = height * 1.5
+                    x_pos -= cellWidth
+                    pointList = (x_pos, y_pos),(x_pos + polysize, y_pos - polysize), (x_pos + cellWidth - polysize, y_pos - polysize), (x_pos + cellWidth, y_pos)
+                    poly = pygame.draw.polygon(self.board, cellColor, pointList)
+                    poly = pygame.draw.polygon(self.board, Colors.BLACK, pointList, 1)
+                    # Store pointList in building object
+                    buildings[building].setPointList(pointList)
+                    
+                rect = pygame.draw.rect(self.board, cellColor, (x_pos, y_pos, cellWidth, cellHeight))
+                rect = pygame.draw.rect(self.board, Colors.BLACK, (x_pos, y_pos, cellWidth, cellHeight),margin)
+                # Store rect in building object
+                buildings[building].setRect(rect)
+                                
+            # Side 4 (Left of screen)
+            elif seq / 8.0 >= 3:
+                x_pos = 0 + padWidth
+                side = 4
+                if seq % 8 == 0:
+                    cellWidth = width * 1.5
+                    cellHeight = height * 1.5
+                else:
+                    cellWidth = width * 1.5
+                    cellHeight = height
+                    y_pos -= cellHeight
+                    pointList = (x_pos + cellWidth, y_pos),(x_pos + cellWidth + polysize, y_pos + polysize), (x_pos + cellWidth + polysize, y_pos - polysize + cellHeight), (x_pos + cellWidth, y_pos + cellHeight)
+                    poly = pygame.draw.polygon(self.board, cellColor, pointList)
+                    poly = pygame.draw.polygon(self.board, Colors.BLACK, pointList, 1)
+                    # Store pointList in building object
+                    buildings[building].setPointList(pointList)
+                    
+                rect = pygame.draw.rect(self.board, cellColor, (x_pos, y_pos, cellWidth, cellHeight))
+                rect = pygame.draw.rect(self.board, Colors.BLACK, (x_pos, y_pos, cellWidth, cellHeight),margin)
+                # Store rect in building object
+                buildings[building].setRect(rect)
+                
+
+    def getGB(self):
+            return self.board
+
+    def colorBuilding(self, building):
+        color = building.getColor()
+        rect = building.getRect()
+        name = building.getName()
+        pointList = building.getPointList()
+        #pygame.draw.rect(self.board, color, rect)
+        pygame.draw.polygon(self.board, color, pointList)
+        pygame.draw.polygon(self.board, color, pointList, 1)
+        self.board.blit(self.boardfont.render(name, True, Colors.BLACK), rect)
+
+
+def main():
+    
+    buildingsFromBuildings = Building.Buildings().getBuildingList()
+    
+    gb = GameBoard(0.65, buildingsFromBuildings)
+    
+    pygame.display.flip()                           #Updates the screen
+    
+    '''
+    gameActive = True
+    while gameActive:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameActive = False
+    pygame.quit()
+    sys.exit()
+    '''
+
+
+if __name__ == '__main__':
+    main()
--- building.py
+++ building.py
@@ -1,88 +1,245 @@
-class Building(object): 
+import Colors
 
-    def __init__(self, name, sequence, purpose, color=(80, 0, 0), price=200000, rent=16000): 
-        
-        MAROON   = (80, 0, 0)               # RGB code for Maroon       (University color - background)
-        LTGRAY   = (200, 200, 200)          # RGB code for light Gray   (Unowned)
-        PURPLE   = (128, 0, 128)            # RGB code for Purple       (Arts & Letters)
-        RED      = (255, 0, 0)              # RGB code for Red          (Business)
-        LTBLUE   = (173, 216, 230)          # RGB code for Light Blue   (Education)
-        ORANGE   = (255, 140, 0)            # RGB code for Orange       (Health & Human Services)
-        PEACOCK  = (0, 128, 128)            # RGB code for Peacock Blue (Humanities & Public Affairs)
-        GOLDEN   = (255, 215, 0)            # RGB code for Golden Yellow (Natural and Applied Science)
-        LTGREEN  = (0, 128, 0)              # RGB code for Green        (Agriculture)
-        BLACK    = (0, 0, 0)                # RGB code for Black        
-        WHITE    = (255, 255, 255)          # RGB code for White        
-        BLUE     = (0, 0, 255)              # RGB code for Blue         
-        GREEN    = (0, 255, 0)              # RGB code for Green
+import pygame
+from pygame.locals import *
+import sys
 
-        self.name = name                # Name of Building
-        self.sequence = sequence        # Sequence number of space
-        self.purpose = purpose          # Purpose of building (academic, sports, support, special)
-        self.color = color              # Color of building (Initially MAROON)
-        self.price = price              # Inital purchase price
-        self.baseGrads = 10             # Base number of grad points earned
-        self.owner = 'Unowned'          # Which dean 'owns' building (initially unowned)
-        self.renovation = False         # Under Renovation (similar to mortgage)
-        self.degreeLvl = 'Associate'    # Current degree level building grants
-        self.costAssociate = 100        # Cost to other deans for associate lvl classes
-        self.costBachelor = 200         # Cost to other deans for bachelor lvl classes
-        self.costMaster = 300           # Cost to other deans for master lvl classes
-        self.costDoctorate = 400        # Cost to other deans for doctorate lvl classes
-        
+class Building(object):
+
+    def __init__(self, name, sequence, purpose):
+        self.name = name            # Name of Building
+        self.sequence = sequence    # Sequence number of space
+        self.purpose = purpose      # Purpose of building (academic, sports, support, special)
+        self.rect = None            # Rect object where its space is located on the board
+
         if purpose == 'academic':
-            self.color = (200, 200, 200)    #LTGRAY
+            self.color = Colors.LIGHTGRAY 
         elif purpose == 'special':
-            self.color = (0, 0, 255)        #BLUE
+            self.color = Colors.BLUE
         elif purpose == 'sports':
-            self.color = (0, 255, 0)        #GREEN
+            self.color = Colors.GREEN 
         elif purpose == 'support':
-            self.color = (255, 255, 255)    #WHITE
-                
+            self.color = Colors.WHITE 
+
     def getName(self): 
         return self.name
 
-    def getPosition(self): 
-        return self.position
-        
-    def setPosition(self, position):
-        self.position = position
-
     def getSequence(self):
-        return self.sequence    
-        
+        return self.sequence
+
     def getPurpose(self):
         return self.purpose
+
+    def getPointList(self): 
+        return self.pointList
         
+    def setPointList(self, pointList):
+        self.pointList = pointList
+
+    def getRect(self): 
+        return self.rect
+        
+    def setRect(self, rect):
+        self.rect = rect   
+
     def getColor(self):
         return self.color
     
     def setColor(self, color):
-        self.color = color
+        self.color = color    
+        
+
+class OwnableBuilding(Building):    
+    def __init__(self, name, sequence, purpose, price=200000):
+        Building.__init__(self, name, sequence, purpose)
+        self.owner = None           # Which dean 'owns' building
+        self.price = price          # Inital purchase price
+        self.renovation = False     # Under Renovation (similar to mortgage)
 
     def getPrice(self): 
-        return self.price 
+        return self.price
 
-    def getBaseGrads(self):
-        return self.baseGrads
-    
     def getOwner(self):
         return self.owner
     
     def setOwner(self, owner):
         self.owner = owner
-        
+
     def getRenovation(self): 
         return self.renovation
         
-    def setRenovation(self):            # Toggles value of renovation (TRUE/FALSE)
-        if self.renovation == FALSE:
-            self.renovation = TRUE
-        elif self.renovation == TRUE:
-            self.renovation = FALSE
+    def setRenovation(self):            # Toggles value of renovation (True/False)
+        if self.renovation == False:
+            self.renovation = True
+        elif self.renovation == True:
+            self.renovation = False    
+        
+
+class NonAcademicBuilding(OwnableBuilding):
+
+    def __init__(self, name, sequence, purpose, price=200000):
+        OwnableBuilding.__init__(self, name, sequence, purpose)
+        self.costNonAcademic = 10000     # Cost to other deans for non-academic building fees
+
+    def getFeeAmount(self):
+        return self.costNonAcademic
+
+    
+class AcademicBuilding(OwnableBuilding):
+
+    def __init__(self, name, sequence, price=200000):
+        OwnableBuilding.__init__(self, name, sequence, 'academic')
+        self.degreeLvl = 'Associate'
+        self.baseGrads = 10             # Base number of grad points earned
+        self.costAssociate = 10000      # Cost to other deans for associate lvl classes
+        self.costBachelor = 20000       # Cost to other deans for bachelor lvl classes
+        self.costMaster = 30000         # Cost to other deans for master lvl classes
+        self.costDoctorate = 40000      # Cost to other deans for doctorate lvl classes
 
     def getDegreeLvl(self):
         return self.degreeLvl
         
     def setDegreeLvl(self, degLvl):
         self.degreeLvl = degLvl
+
+    def getFeeAmount(self):
+        if self.degreeLvl == 'Associate':
+            return self.costAssociate
+        elif self.degreeLvl == 'Bachelor':
+            return self.costBachelor
+        elif self.degreeLvl == 'Master':
+            return self.costMaster
+        elif self.degreeLvl == 'Doctorate':
+            return self.costDoctorate
+
+
+class Buildings(object):
+    """ This class contains a definitive list of the buildings """
+    # These have been reordered to reflect the new board layout.
+
+    def __init__(self):
+
+        self.buildings = []
+        
+        self.buildings.append( Building('Carrington Hall',
+                                        len(self.buildings), 'special') )
+        
+        self.buildings.append( AcademicBuilding('Siceluff Hall',
+                                        len(self.buildings)) )
+        
+        self.buildings.append( AcademicBuilding('Cheek Hall',
+                                        len(self.buildings)) )
+        
+        self.buildings.append( NonAcademicBuilding('University Bookstore',
+                                        len(self.buildings), 'support') )
+        
+        self.buildings.append( NonAcademicBuilding('Hammons Field',
+                                        len(self.buildings), 'sports') )
+        
+        self.buildings.append( AcademicBuilding('Greenwood Lab School',
+                                        len(self.buildings)) )
+        
+        self.buildings.append( NonAcademicBuilding('Foster Recreation Center',
+                                        len(self.buildings), 'support') )
+        
+        self.buildings.append( NonAcademicBuilding('Juanita K Hammons',
+                                        len(self.buildings), 'support') )
+        
+        self.buildings.append( Building('Bear Park North',
+                                        len(self.buildings), 'special') )
+        
+        self.buildings.append( NonAcademicBuilding('Hammons Student Center',
+                                        len(self.buildings), 'support') )
+        
+        self.buildings.append( AcademicBuilding('Brick City',
+                                        len(self.buildings)) )
+        
+        self.buildings.append( AcademicBuilding('Kings St Annex',
+                                        len(self.buildings)) )
+        
+        self.buildings.append( NonAcademicBuilding('JQH Arena',
+                                        len(self.buildings), 'sports') )
+        
+        self.buildings.append( NonAcademicBuilding('Forsythe Athletics Center',
+                                        len(self.buildings), 'support') )
+        
+        self.buildings.append( AcademicBuilding('McDonald Arena',
+                                        len(self.buildings)) )
+        
+        self.buildings.append( NonAcademicBuilding('Plaster Student Union',
+                                        len(self.buildings), 'support') )
+        
+        self.buildings.append( Building('Bear Park South',
+                                        len(self.buildings), 'special') )
+        
+        self.buildings.append( NonAcademicBuilding('Meyer Library',
+                                        len(self.buildings), 'support') )
+        
+        self.buildings.append( AcademicBuilding('Glass Hall',
+                                        len(self.buildings)) )
+        
+        self.buildings.append( AcademicBuilding('Strong Hall',
+                                        len(self.buildings)) )
+        
+        self.buildings.append( NonAcademicBuilding('Allison South Stadium',
+                                        len(self.buildings), 'sports') )
+        
+        self.buildings.append( AcademicBuilding('Kemper Hall',
+                                        len(self.buildings)) )
+        
+        self.buildings.append( AcademicBuilding('Temple Hall',
+                                        len(self.buildings)) )
+        
+        self.buildings.append( NonAcademicBuilding('Central Stores & Maintenance',
+                                        len(self.buildings), 'support') )
+        
+        self.buildings.append( Building('Accreditation Review',
+                                        len(self.buildings), 'special') )
+
+        self.buildings.append( NonAcademicBuilding('Power House',
+                                        len(self.buildings), 'support') )
+        
+        self.buildings.append( AcademicBuilding('Karls Hall',
+                                        len(self.buildings)) )
+        
+        self.buildings.append( AcademicBuilding('Craig Hall',
+                                        len(self.buildings)) )
+        
+        self.buildings.append( NonAcademicBuilding('Plaster Sports Complex',
+                                        len(self.buildings), 'sports') )
+        
+        self.buildings.append( AcademicBuilding('Ellis Hall',
+                                        len(self.buildings)) )
+        
+        self.buildings.append( AcademicBuilding('Hill Hall',
+                                        len(self.buildings)) )
+
+        self.buildings.append( AcademicBuilding('Pummill Hall',
+                                        len(self.buildings)) )
+
+
+    def getBuildingList(self):
+        return self.buildings
+
+    def getNumBuildings(self):
+        return len(self.buildings)
+
+
+def main():
+    # Testing
+    buildings = Buildings().getBuildingList()
+
+    print(Buildings().getNumBuildings())
+
+    for building in range(len(buildings)):
+        print(buildings[building].getName())
+        print(buildings[building].getSequence(), "   /8: ", buildings[building].getSequence()/8, "   %8: ", buildings[building].getSequence()%8)
+        #print(buildings[building].getPosition())
+        print(buildings[building].getPurpose())
+        print(buildings[building].getRect())
+        print(buildings[building].getColor())
+        print()
+
+
+if __name__ == '__main__':
+    main()        
