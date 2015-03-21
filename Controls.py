58f2b4b1c566e1c83716e75f0b3af26bce5acd05
Jennifer Burman <jburman28@gmail.com>
2015-03-21 08:31:11 -0500

    Changed color of controls from white to light gray.

--- Controls.py
+++ Controls.py
@@ -1,46 +1,45 @@
 import pygame, sys
 from pygame.locals import *
-
-
-
+import Colors
 
 class Controls:
 
     def __init__(self, parent, rect):
-        self._area = parent.subsurface(rect)
+        self.area = parent.subsurface(rect)
         #print(rect, parent)
-        self._width = self._area.get_width()
-        self._height = self._area.get_height()
-        button_w_h = (self._width/4, self._height)
-        self._area.fill((0x22, 0x44, 0x66, 0xFF))
-        self._menu = Button(self._area, Rect((0,0,)+button_w_h), "Menu") 
-        self._roll = Button(self._area, Rect((self._width/4,0,)+button_w_h), "Roll")
-        self._trade = Button(self._area, Rect((self._width*2/4,0)+button_w_h), "Trade")
-        self._help = Button(self._area, Rect((self._width*3/4,0)+button_w_h), "Help") 
+        self.width = self.area.get_width()
+        self.height = self.area.get_height()
+        buttonwh = (self.width/4, self.height)##button width and height
+        self.area.fill((0x22, 0x44, 0x66, 0xFF))
+        self.menu = Button(self.area, Rect((0,0,) + buttonwh), "Menu") 
+        self.roll = Button(self.area, Rect((self.width/4,0,) + buttonwh), "Roll")
+        self.trade = Button(self.area, Rect((self.width*2/4,0) + buttonwh), "Trade")
+        self.help = Button(self.area, Rect((self.width*3/4,0) + buttonwh), "Help") 
 
-    def get_width(self):
-        return self._width
+    def getWidth(self):
+        return self.width
 
-    def get_height(self): return self._height   
+    def getHeight(self):
+        return self.height   
 
 
 class Button:
 
-    def __init__(self, parent, rect, label = "", bgcolor=(255, 255, 255), fontcolor=(0, 0, 0)):
+    def __init__(self, parent, rect, label = "", bgcolor=Colors.LIGHTGRAY, fontcolor=(0, 0, 0)):
 
-        self._area = parent.subsurface(rect)
-        self._font = pygame.font.Font( None, 30)
-        self._width = self._area.get_width()
-        self._height = self._area.get_height()
+        self.area = parent.subsurface(rect)
+        self.font = pygame.font.Font( None, 30)
+        self.width = self.area.get_width()
+        self.height = self.area.get_height()
         
 
-        pygame.draw.rect(self._area, bgcolor, (0,0, self._area.get_width(),
-                                              self._area.get_height()))
-        self._textarea = self._font.render( label, 1, fontcolor)
-        self._area.blit(self._textarea,((self._width-self._textarea.get_width())/2,
-                                        (self._height - self._textarea.get_height())/2))
-        pygame.draw.rect(self._area,(0,0,0), (0,0, self._area.get_width(),
-                                              self._area.get_height()), 5)
+        pygame.draw.rect(self.area, bgcolor, (0,0, self.area.get_width(),
+                                              self.area.get_height()))
+        self.textarea = self.font.render( label, 1, fontcolor)
+        self.area.blit(self.textarea,((self.width-self.textarea.get_width())/2,
+                                        (self.height - self.textarea.get_height())/2))
+        pygame.draw.rect(self.area,(0,0,0), (0,0, self.area.get_width(),
+                                              self.area.get_height()), 5)
         #print(rect, parent)
        
         
